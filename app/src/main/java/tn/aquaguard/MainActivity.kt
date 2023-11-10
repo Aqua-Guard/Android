1392a01b1e47f11fb50ff3b1330995c5c631c2d5
Youssefesprit <youssef.farhat@esprit.tn>
2023-11-10 02:50:16 +0100

    ToolBar and DrawerLayout

--- app/src/main/java/tn/aquaguard/MainActivity.kt
+++ app/src/main/java/tn/aquaguard/MainActivity.kt
@@ -1,12 +1,35 @@
 package tn.aquaguard
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var drawerLayout: DrawerLayout ;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar) //Ignore red line errors
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.open_nav,
+            R.string.close_nav
+        )
+
+        // Set the color of the menu icon
+        toggle.drawerArrowDrawable.color = ContextCompat.getColor(this, R.color.white)
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
 
     }
 }
--- app/src/main/java/tn/aquaguard/SplashScreen4.kt
+++ app/src/main/java/tn/aquaguard/SplashScreen4.kt
@@ -1,12 +1,21 @@
 package tn.aquaguard
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SplashScreen4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen4)
+
+        val btnSignIn = findViewById<Button>(R.id.signIn)
+
+        btnSignIn.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
