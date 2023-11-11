21d4b608cd467eb0bd60c0e0737d828d93393521
malek-labidi <labidimalek@esprit.tn>
2023-11-11 04:20:25 +0100

    implement detail event interface

---
+++ app/src/main/java/tn/aquaguard/ui/DetailEventActivity.kt
@@ -0,0 +1,12 @@
+package tn.aquaguard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tn.aquaguard.R
+
+class DetailEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_event)
+    }
+}
