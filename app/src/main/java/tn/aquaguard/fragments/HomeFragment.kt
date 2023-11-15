89bcb865f3931dff4a6b11dbcc9f33af76c84b7b
ademseddik <adem.seddik@esprit.tn>
2023-11-15 11:06:26 +0100

    commit

--- app/src/main/java/tn/aquaguard/fragments/HomeFragment.kt
+++ app/src/main/java/tn/aquaguard/fragments/HomeFragment.kt
@@ -1,16 +1,33 @@
 package tn.aquaguard.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import tn.aquaguard.R
+import tn.aquaguard.adapters.ActualitesAdapter
+import tn.aquaguard.databinding.FragmentHomeBinding
+import tn.aquaguard.models.Actualites
 
 class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding.rvactualite.adapter= ActualitesAdapter(getListNews(requireContext()))
+
+        binding.rvactualite.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        return binding.root
+    }
+    private fun getListNews(context: Context) : MutableList<Actualites> {
+        return mutableListOf(
+            Actualites("bilel01", "The season is coming...","The best time ever for hunting Mannani fish", R.drawable.act, "helo helo",12),
+            Actualites("jalel02", "The season is coming...","The best time ever for hunting Mannani fish", R.drawable.act, "helo helo",12),
+            Actualites("thebestuser", "The season is coming...","The best time ever for hunting Mannani fish", R.drawable.act, "helo helo",12),
+        )
     }
 
 }
---
+++ app/src/main/java/tn/aquaguard/models/Actualites.kt
@@ -0,0 +1,13 @@
+package tn.aquaguard.models
+
+import java.util.Date
+
+data class Actualites (
+    val userId:String,
+    val title:String,
+    val description:String,
+    val image:Int,
+    val text:String,
+    val views:Long
+
+)
