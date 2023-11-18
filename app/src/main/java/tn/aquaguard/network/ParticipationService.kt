304ee822b69442842cc54340d4944b81f64dddd0
malek-labidi <labidimalek@esprit.tn>
2023-11-18 22:30:42 +0100

    implement add participation +verify if is participated + display calendar

---
+++ app/src/main/java/tn/aquaguard/network/ParticipationService.kt
@@ -0,0 +1,20 @@
+package tn.aquaguard.network
+
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+import tn.aquaguard.models.Participation
+
+interface ParticipationService {
+
+    @GET("participations/user/{userId}")
+    suspend fun getParticipations(@Path("userId") userId:String): Response<List<Participation>>
+
+    @POST("participations/participate/{eventId}")
+    suspend fun  addParticipation(@Path("eventId") eventId:String): Response<String>
+
+    @GET("participations/participate/{eventId}")
+    suspend fun isParticipated(@Path("eventId") eventId: String?): Response<Boolean>
+
+}
