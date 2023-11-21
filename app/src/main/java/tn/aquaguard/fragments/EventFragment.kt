f7f89d8854602618ee7334105e8456718571aab7
malek-labidi <labidimalek@esprit.tn>
2023-11-21 23:41:25 +0100

    Handling Empty Events, My Events, and My Calendar Cases

--- app/src/main/java/tn/aquaguard/fragments/EventFragment.kt
+++ app/src/main/java/tn/aquaguard/fragments/EventFragment.kt
@@ -10,6 +10,7 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import tn.aquaguard.R
 import tn.aquaguard.adapters.EventAdapter
+import tn.aquaguard.adapters.MyPostAdapter
 import tn.aquaguard.databinding.FragmentEventsBinding
 import tn.aquaguard.viewmodel.EventViewModel
 
@@ -24,12 +25,26 @@ class EventFragment : Fragment() {
         binding = FragmentEventsBinding.inflate(layoutInflater)
 
 
+        binding.rvEvents.adapter = EventAdapter(emptyList(),viewModel, this)
         binding.rvEvents.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
         viewModel.events.observe(viewLifecycleOwner) { events ->
 
             println("Event API :"+events.toString())
-            binding.rvEvents.adapter = EventAdapter(events,viewModel,this)
+            if (events.isEmpty()) {
+                // Show image and text for empty events list
+                binding.emptyEventsImageView.visibility = View.VISIBLE
+                binding.noevents.visibility = View.VISIBLE
+                binding.rvEvents.visibility = View.GONE
+            } else {
+                // Hide image and text if events list is not empty
+                binding.emptyEventsImageView.visibility = View.GONE
+                binding.noevents.visibility = View.GONE
+                binding.rvEvents.visibility = View.VISIBLE
+
+                // Set up the RecyclerView adapter with non-empty events list
+                binding.rvEvents.adapter = EventAdapter(events, viewModel, this)
+            }
         }
 
         binding.swipeRefreshLayout.setOnRefreshListener {
--- app/src/main/java/tn/aquaguard/fragments/MyCalenderFragment.kt
+++ app/src/main/java/tn/aquaguard/fragments/MyCalenderFragment.kt
@@ -69,6 +69,10 @@ class MyCalenderFragment : Fragment() {
                 val clickedParticipations = participationViewModel.participations.value
                     ?.filter { it.DateEvent == clickedDate }
 
+                if (clickedParticipations.isNullOrEmpty()) {
+                    // No participations for the clicked date, do nothing or show a message
+                    return
+                }
                 // Show the dialog with all events for the clicked date
                 clickedParticipations?.let {
                     showEventDialog(it)
